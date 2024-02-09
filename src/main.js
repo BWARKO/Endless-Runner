c63234a91b49ae865c9325fa878227650ce5228b
Blake Warkenton <bwarkent@ucsc.edu>
2024-02-08 19:28:27 -0800

    init push

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Game: ROT
+// Name: Blake Warkenton
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 800,
+    render: {
+        pixelArt: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Load, Menu, Play ]
+}
+
+let game = new Phaser.Game(config)
---
+++ src/scenes/Load.js
@@ -0,0 +1,34 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super("loadScene")
+    }
+
+    preload() {
+        let loadingBar = this.add.graphics()
+        this.load.on('progress', (value) => {
+            loadingBar.clear()                              // reset fill/line style
+            loadingBar.fillStyle(0xFFFFFF, 1)               // (color, alpha)
+            loadingBar.fillRect(0, centerY, w * value, 5)   // (x, y, w, h)
+        })
+        this.load.on('complete', () => {
+            loadingBar.destroy()
+        })
+
+        this.load.path = './assets/'
+        // load sprite assets
+        this.load.spritesheet()
+        // load audio assets
+        this.load.audio()
+        // load font
+        this.load.bitmapFont('gem', 'font/gem.png', 'font/gem.xml')
+
+    }
+
+    create() {
+        // check for local storage browser support
+        window.localStorage ? console.log('Local storage supported') : console.log('Local storage not supported')
+
+        // go to Title scene
+        this.scene.start('titleScene')
+    }
+}
